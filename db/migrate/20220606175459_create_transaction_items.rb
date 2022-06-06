e99c08e702ea0cc839b48b7e7355bb1cbdc5ab83
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-06 13:27:02 -0500

    Set the database table's attributes to null:false

--- db/migrate/20220606175459_create_transaction_items.rb
+++ db/migrate/20220606175459_create_transaction_items.rb
@@ -1,8 +1,8 @@
 class CreateTransactionItems < ActiveRecord::Migration[7.0]
   def change
     create_table :transaction_items do |t|
-      t.string :name
-      t.decimal :amount, precision: 10, scale: 2
+      t.string :name, null: false
+      t.decimal :amount, precision: 10, scale: 2, null: false
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
