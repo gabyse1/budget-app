ace97d2bd8bfa2c61dcea919a6248d9925624072
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-06 15:59:02 -0500

    Create the Category model associations and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id', inverse_of: :categories
+  has_many :transaction_item, class_name: 'TransactionItem', foreign_key: 'category_id', inverse_of: :category, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
