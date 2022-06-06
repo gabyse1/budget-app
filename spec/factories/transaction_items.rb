30ec068362fb14852f30a9c23d70a6c3476eb6f8
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-06 13:35:04 -0500

    Fix linter bugs

--- spec/factories/transaction_items.rb
+++ spec/factories/transaction_items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction_item do
-    name { "MyString" }
-    amount { "9.99" }
+    name { 'MyString' }
+    amount { '9.99' }
     user { nil }
     category { nil }
   end
