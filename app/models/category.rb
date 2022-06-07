4f1b461cb81ce1ce0cdb07192a590418d9eb36de
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-07 10:07:17 -0500

    Create new migrations for ActiveStorage database tables, and add them to the Category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,17 @@
 class Category < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id', inverse_of: :categories
-  has_many :transaction_item, class_name: 'TransactionItem', foreign_key: 'category_id', inverse_of: :category,
+  has_many :transaction_items, class_name: 'TransactionItem', foreign_key: 'category_id', inverse_of: :category,
                               dependent: :destroy
+  has_one_attached :icon, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
+
+  def category_total_amount
+    total_amount = 0
+    self.transaction_items.each do |ct|
+      total_amount += ct.amount
+    end
+    total_amount
+  end
 end
