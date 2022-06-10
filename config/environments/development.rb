3dc41995f43c1a83d2b581e922e9369fbc483964
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-09 21:54:23 -0500

    Set development action_mailer in development.rb file.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,22 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: Rails.application.credentials.GMAIL_USERNAME }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.GMAIL_USERNAME,
+    password: Rails.application.credentials.GMAIL_PASSWORD,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
