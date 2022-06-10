9ed7b405524e404fc0a86ec9efaad48b1d3857aa
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-09 21:54:45 -0500

    Set production action_mailer in production.rb file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://budgetappgaby.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: Rails.application.credentials.GMAIL_USERNAME }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.GMAIL_USERNAME,
+    password: Rails.application.credentials.GMAIL_PASSWORD,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
