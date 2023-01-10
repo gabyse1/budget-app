e164c29062d079ee466cec0ed30cb7e2b6867032
Gaby <gaby.es.sd@gmail.com>
2023-01-10 11:20:55 -0500

    Update dev.entrypoint file.

--- entrypoints/dev-entrypoint.sh
+++ entrypoints/dev-entrypoint.sh
@@ -12,6 +12,5 @@ if [ -f $APP_PATH/tmp/pids/server.pid ]; then
   rm $APP_PATH/tmp/pids/server.pid
 fi
 
-bundle exec rake assets:precompile
 # run anything by prepending bundle exec to the passed command
 bundle exec ${@}
